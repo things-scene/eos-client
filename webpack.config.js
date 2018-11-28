5b0c80167ea140a8ba5b2ef681dd478d4277444b
Hearty Oh <heartyoh@hatiolab.com>
2018-11-29 01:18:38 +0900

    upgrade things-shell

--- webpack.config.js
+++ webpack.config.js
@@ -29,22 +29,7 @@ module.exports = {
     rules: [{
       test: /\.js$/,
       exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              'env', {
-                targets: {
-                  browsers: ['last 2 Chrome versions', 'Safari 10']
-                },
-                debug: true
-              }
-            ]
-          ],
-          plugins: []
-        }
-      }
+      loader: 'babel-loader'
     }, {
       test: /\.(gif|jpe?g|png)$/,
       loader: 'url-loader?limit=25000',
