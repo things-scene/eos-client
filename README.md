d305b3f5c4336fa263a1f8098f37d6f517e45e3a
Hearty Oh <heartyoh@hatiolab.com>
2019-02-20 16:48:07 +0900

    fix README

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 ## node package를 설치한다.
-```$ yarn```
+
+`$ yarn`
 
 ## 실행
-```$ yarn serve```
-```$ yarn serve:dev```
+
+`$ yarn serve`
+`$ yarn serve:dev`
 
 ## 포트를 바꾸려면, -p 3001 식으로 추가해준다.
-```$ yarn serve```
-```$ yarn serve -p 3001```
+
+`$ yarn serve`
+`$ yarn serve -p 3001`
 
 ## test in browser
+
 http://localhost:3000
 
 ## build
-```$ yarn build```
+
+`$ yarn build`
+
+| type | filename                      | for            | tested |
+| ---- | ----------------------------- | -------------- | ------ |
+| UMD  | things-scene-eos-client.js    | modern browser | X      |
+| UMD  | things-scene-eos-client-ie.js | ie 11          | X      |
+| ESM  | things-scene-eos-client.mjs   | modern browser | X      |
+
 ## publish
-```$ yarn publish```
+
+`$ yarn publish`
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,71 @@
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
-const WebpackCleanupPlugin = require('webpack-cleanup-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require("webpack");
+const WebpackCleanupPlugin = require("webpack-cleanup-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    "things-scene-eos": ['./src/index.js']
+    "things-scene-eos": ["./src/index.js"]
   },
   output: {
-    path: path.resolve('./dist'),
-    filename: '[name].js',
+    path: path.resolve("./dist"),
+    filename: "[name].js"
   },
   resolve: {
-    modules: ['./node_modules']
+    modules: ["./node_modules"]
   },
   resolveLoader: {
-    modules: ['./node_modules']
+    modules: ["./node_modules"]
   },
   externals: {
     "@hatiolab/things-scene": "scene"
   },
   optimization: {
-    minimize: true,
+    minimize: true
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.(gif|jpe?g|png)$/,
-      loader: 'url-loader?limit=25000',
-      query: {
-        limit: 10000,
-        name: '[path][name].[hash:8].[ext]'
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: {
+                      ie: 11
+                    }
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|jpe?g|png)$/,
+        loader: "url-loader?limit=25000",
+        query: {
+          limit: 10000,
+          name: "[path][name].[hash:8].[ext]"
+        }
+      },
+      {
+        test: /\.(obj|mtl|tga|3ds|max|dae)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
       }
-    }, {
-      test: /\.(obj|mtl|tga|3ds|max|dae)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {}
-      }]
-    }]
+    ]
   },
   plugins: [
     new WebpackCleanupPlugin(),
@@ -51,5 +73,5 @@ module.exports = {
       test: /\-min\.js$/
     })
   ],
-  devtool: 'cheap-module-source-map'
-}
+  devtool: "cheap-module-source-map"
+};
